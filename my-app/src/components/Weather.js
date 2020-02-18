fa2a93f97b18d02c5681cdd0b97cb5b762280d88
unknown <nealdeszuu@gmail.com>
2020-02-18 17:05:02 +0100

    feat(weather): add description

--- my-app/src/components/Weather.js
+++ my-app/src/components/Weather.js
@@ -1,9 +1,25 @@
-import React from 'react'
-
+import React, { useState } from 'react';
+import {mockWeather} from '../mocks/mockWeather';
 function Weather() {
+
+    const [weather,setWeather] = useState(mockWeather);
+
+    function KelvinToCelsius(tempKelvin){
+        return Math.round(tempKelvin - 273.15);
+     }
+     
+     function loadIconWeather(idIcon){
+         return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png";
+     }
+
     return (
         <div>
-           <h1>Météo</h1>
+           <h1>Météo : {weather.name}</h1>
+           <img src= { loadIconWeather(weather.weather[0].icon)}></img>
+
+           <p>{weather.weather[0].description}</p>
+           <p>{KelvinToCelsius (weather.main.temp)} C°</p>
+
         </div>
     )
 }

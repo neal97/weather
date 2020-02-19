49b9c292e880afeeb8e2ccfef752528260f09397
unknown <nealdeszuu@gmail.com>
2020-02-19 15:48:20 +0100

    exo

--- my-app/src/actions/weatherAction.js
+++ my-app/src/actions/weatherAction.js
@@ -1,5 +1,12 @@
 import axios from 'axios'
 
-export function getWeather(){
-    return axios.get("http://api.openweathermap.org/data/2.5/weather?q=london&appid=1c16ee5f8545582c3004d1006025abd4");
+const baseUrl="http://api.openweathermap.org/data/2.5";
+const appId= "&appid=1c16ee5f8545582c3004d1006025abd4";
+export function getWeather(ville){
+    return axios.get(baseUrl+"/weather?q="+ville+appId);
 }
+
+
+
+
+
--- my-app/src/components/Weather.js
+++ my-app/src/components/Weather.js
@@ -6,11 +6,11 @@ function Weather(){
 
     // const [weather,setWeather] = useState(mockWeather);
     const[weather,setWeather] = useState(null);
-  
+    const[ville,setVille] = useState(null);
 
     useEffect(()=>{
         loadWeatherData();
-    },[])
+    },[ville])
     
 
     function KelvinToCelsius(tempKelvin){
@@ -18,27 +18,34 @@ function Weather(){
      }
 
     async function loadWeatherData(){
-        const weatherAjax = await getWeather();
+        const weatherAjax = await getWeather(ville);
         console.log(weatherAjax);
         setWeather(weatherAjax.data)
      }
      
 
-
+function change_ville(ville){
+    setVille(ville);
+}
 
      function loadIconWeather(idIcon){
          return "http://openweathermap.org/img/wn/"+idIcon+"@2x.png";
      }
 
     return (
-        <div>
+        <div> 
+         <input placeholder="Recherchez une ville" onChange={(event)=>{change_ville(event.target.value)}}></input> 
         {weather ?
         <div>
            <h1>Météo : {weather.name}</h1>
            <img src= { loadIconWeather(weather.weather[0].icon)}></img>
 
            <p>{weather.weather[0].description}</p>
-           <p>{KelvinToCelsius (weather.main.temp)} C°</p>
+           <p>Température: {KelvinToCelsius (weather.main.temp)} C°</p>
+           <p>Humidité: {weather.main.humidity}%</p>
+           <p>Vitesse du vent: {weather.wind.speed} m/s</p>
+           <p>Température ressenti: {KelvinToCelsius(weather.main.feels_like)} C°</p>
+         
         </div>
        : <div>
        <h1>Météo en attente de chargement</h1>
